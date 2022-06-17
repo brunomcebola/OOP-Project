4da5f4393dbc16b48c82c770937fd01a9204b14e
rui-abrantes <ruimiguel.abrantes@gmail.com>
2022-06-17 11:19:21 +0100

    Fixed small buggs and added JavaDoc Comments for example

--- modes/Simulator.java
+++ modes/Simulator.java
@@ -11,14 +11,26 @@ public class Simulator implements Mode {
     private int credit;
     ArrayList<Card> myHand;
 
+    /**
+    * It is the constructor of the class Simulator.
+    *
+    * @param  ArrayList<Card> hand being played at the time
+    */
     Simulator(ArrayList<Card> _startingHand ){
         super();
-        rank = 0;
+        rank = 50; // starts high because the lower the rank the better
         myHand = _startingHand;
     }
 
-    private void sortHand(){
-        Collections.sort( myHand, new Comparator<ArrayList<Card>>() {
+    /**
+    * Copies the hand at the time and sorts it, giving an output
+    * of the hand sorted.
+    *
+    * @return      ArrayList<Card> which is the hand sorted
+    */
+    private ArrayList<Card> sortHand(){
+        ArrayList<Card> handSorted = myHand;
+        Collections.sort( handSorted, new Comparator<ArrayList<Card>>() {
             public int compare (Card card1, Card card2) {
                 if(card1.getValue() > card2.getValue()){
                     return 1;
@@ -26,6 +38,8 @@ public class Simulator implements Mode {
                 return -1;
             }
         });
+
+        return handSorted;
     }
 
     @Override
@@ -69,77 +83,74 @@ public class Simulator implements Mode {
         //TODO: STILL NEED THE FUNCTIONS TO SUM WHAT'S MISSING
 
         //check rank here
-        if(checkFlush('S', 5 , 'N') || checkFourOfAKind() || checkFlush('R', 5, 'N') ) return 1;
+        if(checkFlush('S', 5 , 'N').size() != 0 || checkFourOfAKind().size() != 0 || checkFlush('R', 5, 'N').size() != 0 ) return 1;
 
-        else if(checkFlush('R',4,'N') ) return 2;
+        if(checkFlush('R',4,'N').size() != 0 ) return 2;
 
-        else if(checkThreeAces()) return 3;
-
-        //TODO:
-        else if(checkStraight() || checkFlush('N', 5, 'N') || checkFHouse()) return 4;
-
-        else if(checkThreeOfAKind()) return 5;
+        if(checkThreeAces().size() != 0) return 3;
 
         //TODO:
-        else if(checkStraight('S', 4, 'N') ) return 6;
+        if(checkStraight().size() != 0 || checkFlush('N', 5, 'N').size() != 0 || checkFHouse().size() != 0) return 4;
 
+        if(checkThreeOfAKind().size() != 0) return 5;
+
+        //TODO:
+        if(checkStraight('S', 4, 'N').size() != 0 ) return 6;
         
-        else if(checkTwoPair()) return 7;
+        if(checkTwoPair().size() != 0) return 7;
         
-        else if(checkHighPair()) return 8;
+        if(checkHighPair().size() != 0) return 8;
        
-        else if(checkFlush('N',4,'N')) return 9;
+        if(checkFlush('N',4,'N').size() != 0) return 9;
        
-        else if(checkFlush('R',3,'N')) return 10;
+        if(checkFlush('R',3,'N').size() != 0) return 10;
         //TODO:
-        else if(checkFourtoOusideStraight()) return 11;
+        if(checkFourtoOusideStraight().size() != 0) return 11;
         
-        else if(checkPair()) return 12;
+        if(checkPair().size() != 0) return 12;
         
-        
-        else if(checkAKQJUnsuited()) return 13; // AKQJ unsuited
+        if(checkAKQJUnsuited().size() != 0) return 13; // AKQJ unsuited
 
         //TODO:
-        else if(checkThreeToStraightFlush()){
-            if(checkTypeOne()) return 14;
-            else if(checkTypeTwo()) return 20;
+        if(checkThreeToStraightFlush().size() != 0){
+            if(checkTypeOne().size() != 0) return 14;
+            else if(checkTypeTwo().size() != 0) return 20;
             return 27;// type 3
         }
-        else if( checkFourToInsideWithThreeHigh()) return 15;
-
-        else if(checkQJS()) return 16; // GJ suited
-
-        
-        else if(checkFlush('N',2,'H')) return 17;
-        
-        else if(checkTwoSHC()) return 18; // Two suited high cards
-        
         //TODO:
-        else if(checkFourToInsideStraight()){
-            if(checkTwoHighCardsOnStraigh()) return 19;
-            if(checkOneHighCardsOnStraigh()) return 21;
+        if( checkFourToInsideWithThreeHigh().size() != 0) return 15;
+
+        if(checkQJS().size() != 0) return 16; // GJ suited
+
+        if(checkFlush('N',2,'H').size() != 0) return 17;
+        
+        if(checkTwoSHC().size() != 0) return 18; // Two suited high cards        
+        //TODO:
+        if(checkFourToInsideStraight().size() != 0){
+            if(checkTwoHighCardsOnStraigh().size() != 0) return 19;
+            if(checkOneHighCardsOnStraigh().size() != 0) return 21;
             return 32;
         }
         
-        else if(checkKQJUnsuited()) return 22;
+        if(checkKQJUnsuited().size() != 0) return 22;
         
-        else if(checkPairSuits(11, 10, 'S' )) return 23; // check JT suited
+        if(checkPairSuits(11, 10, 'S' ).size() != 0) return 23; // check JT suited
         
-        else if(checkPairSuits(12 , 11 ,'U')) return 24; // check QJ unsuited
+        if(checkPairSuits(12 , 11 ,'U').size() != 0) return 24; // check QJ unsuited
    
-        else if(checkFlush('N',1,'H')) return 25;
+        if(checkFlush('N',1,'H').size() != 0) return 25;
 
-        else if(checkPairSuits(12, 10 , 'S')) return 26; // check QT suited
+        if(checkPairSuits(12, 10 , 'S').size() != 0) return 26; // check QT suited
 
-        else if(checkPairSuits( 13, 11, 'U') || checkPairSuits(13, 12, 'U')) return 28; // check KJ and KQ unsuited
+        if(checkPairSuits( 13, 11, 'U').size() != 0 || checkPairSuits(13, 12, 'U').size() != 0) return 28; // check KJ and KQ unsuited
 
-        else if(checkAce()) return 29;
+        if(checkAce().size() != 0) return 29;
 
-        else if(checkPairSuits(13,10,'S')) return 30; // KT  suited
+        if(checkPairSuits(13,10,'S').size() != 0) return 30; // KT  suited
 
-        else if(checkForRoyalCard()) return 31;
+        if(checkForRoyalCard().size() != 0) return 31;
 
-        else if(checkFlush('N',3,'N')) return 33;
+        if(checkFlush('N',3,'N').size() != 0) return 33;
 
         return 34; // discard all
     }
